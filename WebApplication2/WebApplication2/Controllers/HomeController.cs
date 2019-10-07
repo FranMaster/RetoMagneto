d0fbbad703eaeba47f43ee42bece9ba466cf5027
Fran <franjicohen@gmail.com>
2019-10-06 22:32:03 -0300

    Buscaremos Mas Rapido Que Cerebro
    Adrian

--- WebApplication2/WebApplication2/Controllers/HomeController.cs
+++ WebApplication2/WebApplication2/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace WebApplication2.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
+            ViewBag.Title = "Home Page"; 
 
             return View();
         }
